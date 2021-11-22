5a851464d6412c988f2cd9ef9a1f32712de9435f
AriasDev <73326157+kerwynarias@users.noreply.github.com>
2021-11-22 15:54:11 -0300

    Basico
    Maquetado Basico de formulario de contacto con bootstrap

---
+++ Formulario-boletos-tren/js/app.js
@@ -0,0 +1,13 @@
+//efecto type en texto principal
+// const typed = new Typed('.typed', {
+//     strings:['Viaja',
+//      'Disfruta',
+//      'Sueña'],
+//      loop: true,
+//      typeSpeed: 100,
+//      backSpeed: 50,
+//      backDelay: 2000
+// });
+
+//validaciones
+
